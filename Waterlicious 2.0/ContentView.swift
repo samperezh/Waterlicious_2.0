c16630d1d4894d0b0c5ff6e38ea83b15a8b6e1ac
Samantha Perez Hoffman <samanthaperezhoffman@gmail.com>
2021-08-21 20:13:30 -0400

    Initial Commit

---
+++ Waterlicious 2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Waterlicious 2.0
+//
+//  Created by Samantha Perez on 2021-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Waterlicious 2.0/Waterlicious_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Waterlicious_2_0App.swift
+//  Waterlicious 2.0
+//
+//  Created by Samantha Perez on 2021-08-21.
+//
+
+import SwiftUI
+
+@main
+struct Waterlicious_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
